eac6be1719e10943b0a9c2c4c2f9a20e772e67f2
Rishabh4Jakhar <rishabhjakhar04@gmail.com>
2026-06-04 23:09:38 +0530

    Added yay/Akshit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const users = ["rishabhjakhar04", "AdvikGupta2005", "BhattAkshat", "garvit4356", "udaypandita2005", "RobinHood_1803", "Mokshmalik999", "Eklavya_sharma", "Siddharth_kalra05", "HARDIK_ARORA_16", "tanmaygakhar", "_ishaaann_", "tanishqgoyal470", "ModitMalhotra", "timmiii", "nityaagoel","namit23340","mrfate","RuSKie147","diviirockgod6","aryan23143"];
+const users = ["rishabhjakhar04", "AdvikGupta2005", "BhattAkshat", "garvit4356", "udaypandita2005", "RobinHood_1803", "Mokshmalik999", "Eklavya_sharma", "Siddharth_kalra05", "HARDIK_ARORA_16", "tanmaygakhar", "_ishaaann_", "tanishqgoyal470", "ModitMalhotra", "timmiii", "nityaagoel","namit23340","mrfate","RuSKie147","diviirockgod6","aryan23143", "yay-code"];
 const realNames = {
   "rishabhjakhar04": "Rishabh Jakhar",
   "AdvikGupta2005": "Advik Gupta",
@@ -20,7 +20,8 @@ const realNames = {
   "mrfate": "Avi Avinav",
   "RuSKie147": "Raj S. K.",
   "diviirockgod6": "Divyansh K. G.",
-  "aryan23143": "Aryan Dutt"
+  "aryan23143": "Aryan Dutt",
+  "yay-code": "Akshit Bansal"
 };
 const cards = document.getElementById("profiles");
 let cache = {};
